7166e631e4d706127d0a43dbf154b0e2e61bd2da
gijsdb <gijsdeblauw@gmail.com>
2022-07-29 10:50:59 +1200

    added basic gameover component

---
+++ client/src/components/GameOver.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-screen h-screen absolute bg-black bg-opacity-60 flex items-center justify-center">
+    <div class="bg-black border-white rounded-md p-12 text-white">
+      Game over
+      <p v-show="playerStore.Player.value.game.winner == playerStore.Player.value.id">You win</p>
+      <p v-show="playerStore.Player.value.game.winner !== playerStore.Player.value.id">You lose</p>
+      <button @click="returnToMenu">Menu</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { useGameStore } from "../stores/game.js";
+import { storeToRefs } from "pinia";
+
+const router = useRouter();
+const store = useGameStore();
+
+let playerStore = storeToRefs(store);
+
+const returnToMenu = () => {
+  router.push("/");
+};
+</script>
--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="bg-gradient w-screen h-screen flex items-center justify-center">
-    <div v-show="!playerStore.Player.value.game.full" class="bg-black bg-opacity-50 p-12 rounded-2xl shadow-2xl text-white font-bold text-center">
+    <GameOver v-show="playerStore.Player.value.game.game_over" />
+    <div
+      v-show="!playerStore.Player.value.game.full"
+      class="bg-black bg-opacity-50 p-12 rounded-2xl shadow-2xl text-white font-bold text-center"
+    >
       <p class="text-4xl py-4">Game {{ playerStore.Player.value.game.ID }} - Waiting for players...</p>
       <Loader />
     </div>
@@ -15,8 +19,10 @@
     </div>
     <Dice v-show="playerStore.Player.value.game.full" :clear="enableDice" />
 
-    <div v-show="playerStore.Player.value.game.full" class="flex">
-      <div class="z-40 bg-black bg-opacity-80 border-white border-2 shadow-2xl p-4 w-[40vw] h-[40vw] rounded-lg grid grid-cols-3">
+    <div v-show="playerStore.Player.value.game.full && !playerStore.Player.value.game.game_over" class="flex">
+      <div
+        class="z-40 bg-black bg-opacity-80 border-white border-2 shadow-2xl p-4 w-[40vw] h-[40vw] rounded-lg grid grid-cols-3"
+      >
         <Square v-for="(square, idx) in playerStore.Player.value.game.game_board.squares" :key="idx" :squareIdx="idx" />
       </div>
     </div>
@@ -31,6 +37,7 @@ import { storeToRefs } from "pinia";
 import APIClient from "../APIClient";
 
 import Square from "../components/Square.vue";
+import GameOver from "../components/GameOver.vue";
 import Dice from "../components/Dice.vue";
 import Loader from "../components/Loader.vue";
 
@@ -42,6 +49,7 @@ const { refreshGame } = store;
 let playerStore = storeToRefs(store);
 let enableDice = ref(false);
 let waitForPlayerInterval = null;
+let refreshInterval = null;
 
 onMounted(() => {
   if (!playerStore.Player.value.game.full) {
@@ -51,7 +59,7 @@ onMounted(() => {
   } else {
     clearInterval(waitForPlayerInterval);
   }
-  setInterval(async () => {
+  refreshInterval = setInterval(async () => {
     await refreshGame();
   }, 1000);
 });
@@ -59,6 +67,6 @@ onMounted(() => {
 onBeforeUnmount(async () => {
   // // TODO CALL API TO LEAVE GAME
   clearInterval(waitForPlayerInterval);
-  // clearInterval(waitForTurnInterval);
+  clearInterval(refreshInterval);
 });
 </script>
